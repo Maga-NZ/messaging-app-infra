006e00e5f4b0dd52110474d3918f69db6228927d
Maga-NZ <antoshkievm@gmail.com>
2025-06-01 22:46:11 +1200

    Add jenkins_server definition, fix key name, verify jenkins_sg in Terraform config

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -19,3 +19,17 @@ resource "aws_instance" "private_instance" {
     Name = "PrivateInstance"
   }
 }
+
+resource "aws_instance" "jenkins_server" {
+  ami                    = data.aws_ami.amazon_linux.id  # Use data source for AMI
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet_1.id # Jenkins instance is likely in the public subnet
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id] # Attach to the Jenkins security group
+  key_name               = aws_key_pair.project_key_pair.key_name # Reference the key pair resource
+  tags = {
+    Name = "JenkinsServer"
+  }
+  # You may also need to add the IAM instance profile if it was attached to the Jenkins instance for SSM/ECR access
+  # For example:
+  # iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+}
